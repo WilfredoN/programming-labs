f89522484578ab211a7853a20bf56692799a20bc
Nikita Afanasiev <nikita.afanasyevnn@gmail.com>
2024-10-22 14:11:02 +0300

    feat: added another tests as comments

--- lab_05/main.cpp
+++ lab_05/main.cpp
@@ -3,7 +3,11 @@
 
 int main() {
   // Set 1:
-  double a = 5.0, b = 15.0, c = 2.0, d = 1.0;
+  double a = 2.0, b = 3.0, c = 4.0, d = 5.0;
+  // Set 2:
+  // double a = 3.0, b = 2.0, c = 1.0, d = 4.0;
+  // Set 3:
+  // double a = 5.0, b = 15.0, c = 2.0, d = 1.0;
   double y = (atan(a) / exp(b * cos(c))) + pow(log2(d + 1), 1.0 / 3.0);
   printf("%f\n", y);
   return 0;
