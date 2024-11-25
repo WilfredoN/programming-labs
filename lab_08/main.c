14629f61106530dc0b45e866ae68deff84ff2bf8
WilfredoN <thekillermaps@gmail.com>
2024-11-25 22:47:12 +0200

    refactor: removed size const, refactored structure, makefile

--- lab_08/main.c
+++ lab_08/main.c
@@ -2,19 +2,23 @@
 
 int main()
 {
-    int *array = generate();
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int *array = generate(size);
     printf("Generated array:\n");
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < size; i++)
     {
         printf("%d ", array[i]);
     }
-    printf("\nCount of even numbers in array: %d", countEven(array));
-    printf("\nFirst negative element in array: %d", firstNegative(array));
+    printf("\nCount of even numbers in array: %d", countEven(array, size));
+    printf("\nFirst negative element in array: %d", firstNegative(array, size));
     printf("\nArray rotated to the left:\n");
-    rotateLeft(array);
+    rotateLeft(array, size);
     printf("\nSorted array by absolute value:\n");
-    sortByAbsolute(array);
-    printf("\nMedian of the sorted array: %.2f\n", findMedian(array));
+    sortByAbsolute(array, size);
+    printf("\nMedian of the sorted array: %.2f\n", findMedian(array, size));
 
     free(array);
 
--- lab_08/src/lib.c
+++ lab_08/src/lib.c
@@ -5,29 +5,30 @@ int compare(const void *a, const void *b)
     return (*(int *)a - *(int *)b);
 }
 
-int *generate()
+int *generate(int size)
 {
-    int *array = malloc(sizeof(int) * SIZE);
+    int *array = malloc(sizeof(int) * size);
     srand(time(NULL));
-    for (int i = 0; i < SIZE; i++)
+
+    for (int i = 0; i < size; i++)
     {
         array[i] = rand() % 49 - 10;
     }
     return array;
 }
 
-void print(int *array)
+void print(int *array, int size)
 {
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < size; i++)
     {
         printf("%d ", array[i]);
     }
 }
 
-int countEven(int *array)
+int countEven(int *array, int size)
 {
     int count = 0;
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < size; i++)
     {
         if (array[i] % 2 == 0)
         {
@@ -37,9 +38,9 @@ int countEven(int *array)
     return count;
 }
 
-int firstNegative(int *array)
+int firstNegative(int *array, int size)
 {
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < size; i++)
     {
         if (array[i] < 0)
         {
@@ -49,42 +50,49 @@ int firstNegative(int *array)
     return -1;
 }
 
-void rotateLeft(int *array)
+void rotateLeft(int *array, int size)
 {
-    int temp = array[0];
+    if (size <= 0)
+        return;
 
-    for (int i = 0; i < SIZE - 1; i++)
+    int temp = array[0];
+    for (int i = 0; i < size - 1; i++)
     {
         array[i] = array[i + 1];
     }
-    array[SIZE - 1] = temp;
+    array[size - 1] = temp;
 
-    print(array);
+    print(array, size);
 }
 
-void sortByAbsolute(int *array)
+void sortByAbsolute(int *array, int size)
 {
     int temp;
-    for (int i = 0; i < SIZE - 1; i++)
+    for (int i = 0; i < size - 1; i++)
     {
-        for (int j = 0; j < SIZE - i - 1; j++)
+        for (int j = 0; j < size - i - 1; j++)
+        {
             if (abs(array[j]) > abs(array[j + 1]))
             {
                 temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
             }
+        }
     }
-    print(array);
+    print(array, size);
 }
 
-float findMedian(int *array)
+float findMedian(int *array, int size)
 {
-    qsort(array, SIZE, sizeof(int), compare);
-    if (SIZE % 2 != 0)
+    if (size == 0)
+        return 0.0f;
+
+    qsort(array, size, sizeof(int), compare);
+    if (size % 2 != 0)
     {
-        return array[SIZE / 2];
+        return array[size / 2];
     }
 
-    return (array[SIZE / 2 - 1] + array[SIZE / 2]) / 2.0;
+    return (array[size / 2 - 1] + array[size / 2]) / 2.0f;
 }
